8e701c044f3ca66b95ecdefee17f6202ca25e937
matvey <matgr2n@gmail.com>
2024-03-19 12:08:41 +0300

    changed base settings

--- src/leadtransfer/api/views.py
+++ src/leadtransfer/api/views.py
@@ -5,32 +5,31 @@ from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.response import Response
 
 from .serializers import CRMContactSerializer
-from ..models import CRMContact
-from ..service.amocrm import send_lead_to_amocrm, get_amo_contacts
+from ..service.amocrm import send_lead_to_amocrm
 from ..service.validation import get_lead_validated_data, get_contact_validated_data
 
 
-class SyncContactsAPIView(ListAPIView):
-    serializer_class = CRMContactSerializer
-    queryset = CRMContact.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        data_len = 0
-        page = 1
-        while True:
-            try:
-                contacts = get_amo_contacts(page=page)
-                serializer = CRMContactSerializer(data=contacts, many=True)
-                if serializer.is_valid():
-                    serializer.save()
-                data_len += len(serializer.data)
-            except JSONDecodeError as e:
-                break
-            except Exception as e:
-                print(e)
-            print(page)
-            page += 1
-        return Response(data={"len": data_len}, status=status.HTTP_200_OK)
+# class SyncContactsAPIView(ListAPIView):
+#     serializer_class = CRMContactSerializer
+#     queryset = CRMContact.objects.all()
+#
+#     def get(self, request, *args, **kwargs):
+#         data_len = 0
+#         page = 1
+#         while True:
+#             try:
+#                 contacts = get_amo_contacts(page=page)
+#                 serializer = CRMContactSerializer(data=contacts, many=True)
+#                 if serializer.is_valid():
+#                     serializer.save()
+#                 data_len += len(serializer.data)
+#             except JSONDecodeError as e:
+#                 break
+#             except Exception as e:
+#                 print(e)
+#             print(page)
+#             page += 1
+#         return Response(data={"len": data_len}, status=status.HTTP_200_OK)
 
 
 class LeadTransferAPIView(CreateAPIView):
